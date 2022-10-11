31ef6b2c780bd089519bc02ccc0dedfada3380b5
Chattray Chea <Zavvy-Glitch>
2022-10-11 00:11:52 +0000

    Challenge: Shebang, Input, Print, Concatenate, Variables

---
+++ challenge/challenge1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main(): 
+
+    user_input = input("Please tell me your name:")
+    user_day = input("Could you tell me which day of the week it is?")
+    print("Hello, " + user_input + "! " + "Happy " + user_day + "!")
+
+main()
