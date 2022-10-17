16aa6623e0ba85633d0621800c45d614638a2d69
Chattray Chea <Zavvy-Glitch>
2022-10-17 17:40:39 +0000

    challenge1 updated to use fstring syntax

--- challenge/challenge1.py
+++ challenge/challenge1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-def main(): 
+def main():
 
-    user_input = input("Please tell me your name:")
-    user_day = input("Could you tell me which day of the week it is?")
-    print("Hello, " + user_input + "! " + "Happy " + user_day + "!")
-
+    # input incurs a prompt on CLI for user type in information to be used in string.
+    user_input = input("Please tell me your name: ")
+    user_day = input("Could you tell me which day of the week it is? ")
+    # f'string syntax, allows for direct input of variables into string without need of concatenation
+    print(f"Hello, {user_input}! Happy {user_day}!")
+    
 main()
