78c767abe4156c822a5bed8a556cce3d1e14c9e1
Chattray Chea <Zavvy-Glitch>
2022-10-17 20:13:32 +0000

    adds capitalization to name

--- challenge/challenge2.py
+++ challenge/challenge2.py
@@ -7,5 +7,5 @@ name= input("What is your name?\n>")
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
 
-print(f"Hi {name}! Welcome to Day {mylist[1]} of {mylist[5]} Training!")
+print(f"Hi {name.upper()}! Welcome to Day {mylist[1]} of {mylist[5]} Training!")
 
