1ff7f29c2bdb108cdaaaeff71ed703047d54af32
Chattray Chea <Zavvy-Glitch>
2022-10-17 21:52:57 +0000

    fixed syntax errors, incorrect indentation

--- challenge/challenge3.py
+++ challenge/challenge3.py
@@ -8,42 +8,44 @@ from random import choice
 
 def main():
 
-wordbank= ["indentation", "spaces"]
+    wordbank= ["indentation", "spaces"]
 
-tlgstudents= ["Cat", "Chris", "Dao", "David", "Henwin", "Herman", "Jose", "Justin", "Kris", "Mannie", "Marcos", "Marshall", "Michael", "Mike", "Nikko", "Phil", "Ryan", "Sachin", "Samekh", "Will"]
+    tlgstudents= ["Cat", "Chris", "Dao", "David", "Henwin", "Herman", "Jose", "Justin", "Kris", "Mannie", "Marcos", "Marshall", "Michael", "Mike", "Nikko", "Phil", "Ryan", "Sachin", "Samekh", "Will"]
 
-# this will add the integer 4 to the wordbank list
-wordbank.append(4)
+    # this will add the integer 4 to the wordbank list
+    wordbank.append(4)
 
-# using """three quotes""" creates a multi-line doc string
-# in other words, a string that uses line breaks instead of /n
-print("""Do one of the following:
-        - Enter a number between 0 and 17
-        - Type in a student's name
-        - Type in the word 'random'""")
+    # using """three quotes""" creates a multi-line doc string
+    # in other words, a string that uses line breaks instead of /n
+    print("""Do one of the following:
+            - Enter a number between 0 and 17
+            - Type in a student's name
+            - Type in the word 'random'""")
 
-# save the user's input as the variable "choice"
-selection = input(">")
+    # save the user's input as the variable "choice"
+    selection = input(">")
 
-# if the number entered by the user can be
-# cleanly converted to an integer:
-if selection.isdigit():
-    # convert string to integer and slice the list
-    # save the returned name as "name"
-    name = tlgstudents[int(selection)]
+    # if the number entered by the user can be
+    # cleanly converted to an integer:
+    if selection.isdigit():
 
-# if the name chosen is actually in the list of students:
-elif selection in tlgstudents:
-    # assign that name as the variable "name"
-    name = selection
+        # convert string to integer and slice the list
+        # save the returned name as "name"
+        name = tlgstudents[int(selection)]
 
-else:
-    # if none of the above is true, use the choice()
-    # function to grab a random name and save it as "name"
-    name = choice(tlgstudents)
+    # if the name chosen is actually in the list of students:
+    elif selection in tlgstudents:
+    
+        # assign that name as the variable "name"
+        name = selection
 
-# Use an f-string to neatly combine these elements into a sentence.
-print(f"Your victims name is {name}")
-print(f"{name} always uses {wordbank[2]} {wordbank[1]} for {wordbank[0]}.")
+    else:
+        # if none of the above is true, use the choice()
+        # function to grab a random name and save it as "name"
+        name = choice(tlgstudents)
+
+    # Use an f-string to neatly combine these elements into a sentence.
+    print(f"Your victims name is {name}")
+    print(f"{name} always uses {wordbank[2]} {wordbank[1]} for {wordbank[0]}.")
 
 main()
