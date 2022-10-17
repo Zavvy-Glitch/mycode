63d53c0dec72442ef2e74e6ade67da46daec0e47
Chattray Chea <Zavvy-Glitch>
2022-10-17 19:53:03 +0000

    adds changes to stringmethod 1 & 2. 1 without function. 2 with function

--- strings/stringmethod01.py
+++ strings/stringmethod01.py
@@ -2,12 +2,18 @@
 
 # create a small string
 lilstring = "Alta3 Research offers classes on Python coding"
+
+# will return a list/array with the given string split into individual objects
 newlist = lilstring.split(" ")
+
+# prints or displays contents of newlist on screen
 print(newlist)
 
 # create a list of strings
 myiplist = ["192", "168", "0", "12"]
+
 # set singleip as the result of joining the list my iplist around the "."
 singleip = ".".join(myiplist)
+
 # display singleip
 print(singleip)
--- strings/stringmethod02.py
+++ strings/stringmethod02.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 """Alta3 Research || Author: RZFeeser@alta3.com"""
 
+# initiates function. (We named it main)
 def main():
-    """ Run-time code"""
+  
+    """ Run-time code """
     # create a small string
     lilstring = "Alta3 Research offers classes on Python coding"
-    newlist = lilstring.split(" ") # this returns a list
+    
+    # this returns a list
+    newlist = lilstring.split(" ")
+
     print(newlist)
 
     # create a list of strings
     myiplist = ["192", "168", "0", "12"]
+
     # set singleip as the result of joining the list myiplist around the "."
     singleip = ".".join(myiplist)
+
     # display singleip
     print(singleip)
 
