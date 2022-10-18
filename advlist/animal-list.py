4773a77300049ac7c7fcb794c92a30df2403ee7b
Chattray Chea <Zavvy-Glitch>
2022-10-18 16:44:44 +0000

    manipulating complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" TLG Cohort D23 | CChea
+    Lists within Lists practice """
+
+def main():
+    
+    animList = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    count = [len(i) for i in animList]
+        print(animList[i])
+    print(count)
+
+main()
