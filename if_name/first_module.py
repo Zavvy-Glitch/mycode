fdd005e4025906d33f440ec308a6662ebcd7de81
Chattray Chea <Zavvy-Glitch>
2022-10-19 17:11:40 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" TLG Cohort D23 | CChea
+    adding guards """
+
+print("This code will always execute.")
+
+def main():
+    print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+    main()
---
+++ if_name/second_module.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import first_module
+
+# this line calls a specific function from the imported module
+first_module.main()
+
+print("Module #2 Name = ", __name__)
